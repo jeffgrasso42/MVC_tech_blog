dc46698d818bee7ab10586b385802aca3725fd00
JAG <jagrasso42@gmail.com>
2022-11-08 15:54:58 -0500

    added path and handlebars

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 const express = require('express');
+const path = require('path');
 const session = require('express-session');
 const routes = require('./controllers');
+const exphbs = require('express-handlebars');
 
 const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
+// const helpers = require('./utils/helpers');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -18,10 +21,16 @@ const sess = {
   }),
 };
 
+const hbs = exphbs.create();
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
 app.use(session(sess));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(routes);
 
